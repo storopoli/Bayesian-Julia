fa0aa41a1f7bd5ed3c283168f35db78bb77cb0b2
Jose Storopoli <thestoropoli@gmail.com>
2021-04-05 16:44:32 -0300

    Added Literate.jl

---
+++ _literate/test.jl
@@ -0,0 +1,14 @@
+# # Here is my website
+
+# I'm going to use $\LaTeX$ and **Julia**.
+
+
+# ### Here's some maths
+
+# $ f(x,y) = x^{x} + \frac{y}{x} $
+
+### And some code
+
+f(x,y) = x^x + y / x
+
+f(2,3)
