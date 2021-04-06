adc5978a72c5a58143c9d348f6a937b8a9e98b41
Jose Storopoli <thestoropoli@gmail.com>
2021-04-06 11:39:47 -0300

    math tests

--- _literate/1_why_Julia.jl
+++ _literate/1_why_Julia.jl
@@ -4,13 +4,20 @@
 
 # ## Here's some maths
 
+# @@no-number
 # $$ f(x,y) = x^{x} + \frac{y}{x} $$
+# @@
 
 # And some code
 
 f(x,y) = x^x + y / x
 
 f(2,3)
+
+# Also more math but now labbelled
+# $$ e^{i \pi} + 1 = 0 \label{euler} $$
+
+# Then, we can reference the equation as Equation \eqref{euler}.
 
 # ## Testing Plots
 
