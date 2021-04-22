aade8d957c43be423396aee7924e6e201d5c6795
Jose Storopoli <thestoropoli@gmail.com>
2021-04-22 17:31:18 -0300

    final draft 4_Turing

--- _literate/4_Turing.jl
+++ _literate/4_Turing.jl
@@ -66,7 +66,7 @@
 # * using `=`: which means that a variable does not follow a probability distribution and its value is deterministic (like the normal `=` assignment in programming languages)
 
 # Turing will perform automatic inference on all variables that you specify using `~`.
-# Here is a simple example of how we would model a 6-sided dice. Note that a "fair" dice will be distributed as a discrete uniform
+# Here is a simple example of how we would model a six-sided dice. Note that a "fair" dice will be distributed as a discrete uniform
 # probability with the lower bound as 1 and the upper bound as 6:
 
 # $$ X \sim \text{Uniform}(1,6) \label{uniformdice} $$
@@ -81,7 +81,7 @@ using Plots, StatsPlots, Distributions, LaTeXStrings
 
 dice = DiscreteUniform(1, 6)
 plot(dice,
-    label="6-sided Dice",
+    label="six-sided Dice",
     ms=5,
     xlabel=L"\theta",
     ylabel="Mass",
@@ -91,19 +91,19 @@ vline!([mean(dice)], lw=5, col=:red, label=L"E(\theta)")
 savefig(joinpath(@OUTPUT, "dice.svg")); # hide
 
 # \fig{dice}
-# \center{*A "fair" 6-sided Dice: Discrete Uniform between 1 and 6*} \\
+# \center{*A "fair" six-sided Dice: Discrete Uniform between 1 and 6*} \\
 
 # So let's specify our first Turing model. It will be named `dice_throw` and will have a single parameter `y`
-# which is a N-dimensional vector of integers representing the observed data, *i.e.* the outcomes of $N$ 6-sided dice throws:
+# which is a $N$-dimensional vector of integers representing the observed data, *i.e.* the outcomes of $N$ six-sided dice throws:
 
 using Turing
 
 @model dice_throw(y) = begin
-    #Our prior belief about the probability of each result in a 6-sided dice.
+    #Our prior belief about the probability of each result in a six-sided dice.
     #p is a vector of length 6 each with probability p that sums up to 1.
     p ~ Dirichlet(6, 1)
 
-    #Each outcome of the 6-sided dice has a probability p.
+    #Each outcome of the six-sided dice has a probability p.
     y .~ Categorical(p)
 end;
 
@@ -111,28 +111,34 @@ end;
 # is the multivariate generalization of the [Beta distribution](https://en.wikipedia.org/wiki/Beta_distribution).
 # The Dirichlet distribution is often used as the conjugate prior for Categorical or Multinomial distributions. Since our dice
 # is modelled as a [Categorical distribution](https://en.wikipedia.org/wiki/Categorical_distribution)
-# with 6 possible results $y \in \{ 1, 2, 3, 4, 5, 6 \}$ with some probability vector
-# $\mathbf{p} = (p_1, \dots, p_6)$. Since all outcomes must sum up to 1 to be a valid probability, we impose the constraint that
-# all $p$s must sum up to 1 $\sum^n_{i=1} p_i = 1$. We could use a  vector o 6 Beta distributions but it would be hard and
+# with six possible results $y \in \{ 1, 2, 3, 4, 5, 6 \}$ with some probability vector
+# $\mathbf{p} = (p_1, \dots, p_6)$. Since all mutually exclusive outcomes must sum up to um to be a valid probability, we impose the constraint that
+# all $p$s must sum up to um -- $\sum^n_{i=1} p_i = 1$. We could have used a  vector o six Beta random variables but it would be hard and
 # inefficient to enforce this constraint. Instead, I've opted for a Dirichlet with a weekly informative prior towards a
-# "fair" dice which is encoded as a `Dirichlet(6,1)`. This is translated as a 6-dimensional vector of elements that sum to 1:
+# "fair" dice which is encoded as a `Dirichlet(6,1)`. This is translated as a 6-dimensional vector of elements that sum to one:
 
 mean(Dirichlet(6, 1))
 
+# And, indeed, it sums up to one:
+
+sum(mean(Dirichlet(6, 1)))
+
 # Also, since the outcome of a [Categorical distribution](https://en.wikipedia.org/wiki/Categorical_distribution) is an integer
-# and `y` is a 1,000-dimensional vector of integers we need to apply some sort of broadcasting here. This is
+# and `y` is a $N$-dimensional vector of integers we need to apply some sort of broadcasting here. This is
 # done by adding the familiar dot `.` broadcasting operator in Julia: `y .~ Categorical` this means that all elements of `y` are
 # distributed as a Categorical distribution.
 
-# Now let's set a seed for the pseudo-random number generator and simulate 1,000 throws of a 6-sided dice:
+# ### Simulating Data
+
+# Now let's set a seed for the pseudo-random number generator and simulate 1,000 throws of a six-sided dice:
 
 using Random
 
-Random.seed!(123)
+Random.seed!(123);
 
-data = rand(dice, 1_000); # remember dice = DiscreteUniform(1, 6)
+data = rand(DiscreteUniform(1, 6), 1_000);
 
-# The vector `data` is a 1,000-length vector of `Int`s ranging from 1 to 6, like a regular 6-sided dice outcome would be:
+# The vector `data` is a 1,000-length vector of `Int`s ranging from 1 to 6, just like how a regular six-sided dice outcome would be:
 
 first(data, 5)
 
@@ -140,17 +146,17 @@ first(data, 5)
 
 model = dice_throw(data);
 
-# and apply to it the Turing's `sample()` function that takes a Turing model as a first argument, along with a
+# Next, we call the Turing's `sample()` function that takes a Turing model as a first argument, along with a
 # sampler as the second argument, and the third argument is the number of iterations. Here, I will use the `NUTS()` sampler from
-# `AdvancedHMC.jl` and 2,000 iterations. Please note that as default Turing samplers will discard the first half of iterations as
+# `AdvancedHMC.jl` and 2,000 iterations. Please note that, as default, Turing samplers will discard the first half of iterations as
 # warmup. So the sampler will output 1,000 samples (`floor(2_000 / 2)`):
 
 chain = sample(model, NUTS(), 2_000);
 
 # Now let's inspect the chain. We can do that with the function `describe()` that will return a 2-element vector of
 # `ChainDataFrame` (this is the type defined by `MCMCChains.jl` to store Markov chain's information regarding the inferred
-# parameters). The first `ChainDataFrame` has information regarding the parameters' summary statistics (mean, std, r_hat, ...)
-# and the second is the parameters' quantiles. Since `describe(chain)` returns a 2-element vector, I will output it to two variables:
+# parameters). The first `ChainDataFrame` has information regarding the parameters' summary statistics (`mean`, `std`, `r_hat`, ...)
+# and the second is the parameters' quantiles. Since `describe(chain)` returns a 2-element vector, I will assign the output to two variables:
 
 summaries, quantiles = describe(chain);
 
@@ -158,22 +164,23 @@ summaries, quantiles = describe(chain);
 
 summaries
 
-# Here `p` is a 6-dimensional vector of probabilities, which each one associated with a mutually exclusive outcome of a 6-sided
-# dice throw. As we expected, the probabilities are almost equal to $\frac{1}{6}$, like a "fair" 6-sided dice that we simulated
+# Here `p` is a 6-dimensional vector of probabilities, which each one associated with a mutually exclusive outcome of a six-sided
+# dice throw. As we expected, the probabilities are almost equal to $\frac{1}{6}$, like a "fair" six-sided dice that we simulated
 # data from (sampling from `DiscreteUniform(1, 6)`). Indeed, just for a sanity check, the mean of the estimates of `p` sums up to 1:
 
 sum(summaries[:, :mean])
 
-# And, finally let's compute the $E(\widetilde{X})$ using the standard expectation definition for a discrete random variable:
+# And, finally let's compute the expectation of the estimated six-sided dice, $E(\tilde{X})$, using the standard expectation
+# definition of expectation for a discrete random variable:
 
 # $$ E(X) = \sum_{x \in X} x \cdot P(x) $$
 
 sum([idx * i for (i, idx) in enumerate(summaries[:, :mean])])
 
-# Bingo! The estimated expectation is very close to the theoretical expectation of $\frac{7}{2} = 3.5$ as we've show
+# Bingo! The estimated expectation is very close to the theoretical expectation of $\frac{7}{2} = 3.5$, as we've show
 # in \eqref{expectationdice}.
 
-# ## Conlusion
+# ## Conclusion
 
 # This is the basic overview of Turing usage. You see how simple and intuitive is to specify probabilistic models using Turing?!
 
