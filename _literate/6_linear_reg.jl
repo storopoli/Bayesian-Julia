151879d1a1a79381c6266ce1d87a0cc4a3726f16
Jose Storopoli <thestoropoli@gmail.com>
2021-05-02 13:29:53 -0300

    draft 6_linear_reg

--- _literate/6_linear_reg.jl
+++ _literate/6_linear_reg.jl
@@ -4,6 +4,27 @@
 # > \\ \\
 # > George Box (Box, 1976)
 
+# This tutorial begins with a very provocative quote from the statistician
+# [George Box](https://en.wikipedia.org/wiki/George_E._P._Box) (figure below)
+# on statistical models. Yes, all models are somehow wrong. But they are very useful.
+# The idea is that the reality is too complex for us to understand when analyzing it in
+# a naked and raw way. We need to somehow simplify it into individual components and analyze
+# their relationships. But there is a danger here: any simplification of reality promotes loss
+# of information in some way. Therefore, we always have a delicate balance between simplifications
+# of reality through models and the inherent loss of information. Now you ask me:
+# "how are they useful?" Imagine that you are in total darkness and you have a very powerful
+# flashlight but with a narrow beam. Are you going to throw the flashlight away because it
+# can't light everything around you and stay in the dark? You must use the flashlight to aim
+# at interesting places in the darkness in order to illuminate them. You will never find a
+# flashlight that illuminates everything with the clarity you need to analyze all the fine details
+# of reality. Just as you will never find a unique model that will explain the whole reality around
+# you. You need different flashlights just like you need different models. Without them you will
+# be in total darkness.
+
+# ![George Box](/pages/images/george_box.jpg)
+#
+# \center{*George Box*} \\
+
 # ## Footnotes
 
 # ## References
