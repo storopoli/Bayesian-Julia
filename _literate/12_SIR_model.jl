0d4cff53bf52824673b58c012102cf1f03c0c8ed
Jose Eduardo Storopoli <thestoropoli@gmail.com>
2021-06-13 20:03:27 -0300

    updated SIR stuff

--- _literate/12_SIR_model.jl
+++ _literate/12_SIR_model.jl
@@ -1,7 +1,15 @@
-# # Bonus: SIR Model
+# # Bonus: Epidemiological Models using ODE Solvers in Turing
 
-# WIP!
-# using Brazil's data
+# Ok, now this is something that really makes me very excited with Julia's
+# ecossystem. If you want to use an **O**rdinary **D**ifferential **E**quation solver
+# in your Turing model, you don't need to code it from scratch. You've just
+# **borrow a pre-made one from [`DifferentialEquations.jl`](https://diffeq.sciml.ai/dev/)**.
+# This is what makes Julia so great. We can use functions and types
+# defined in other packages into another package and it will probably work either
+# straight out of the bat or without much effort!
+
+# For this tutorial I'll be using Brazil's COVID data from the [Media Consortium](https://brasil.io/covid19/).
+# For reproducibility, we'll restrict the data to the year of 2020:
 
 using Downloads, DataFrames, CSV, GZip, Chain, Dates
 
@@ -9,10 +17,10 @@ url = "https://data.brasil.io/dataset/covid19/caso_full.csv.gz"
 file = Downloads.download(url)
 df = CSV.File(GZip.open(file, "r")) |> DataFrame
 
-# Getting only national-level data in 2021
+# Getting only national-level data in 2020
 
 br = @chain df begin
-    filter([:date, :city] => (date, city) -> date > Dates.Date("2021-01-01") && ismissing(city), _)
+    filter([:date, :city] => (date, city) -> date < Dates.Dajuliate("2021-01-01") && ismissing(city), _)
     groupby(:date)
     combine(
         [:estimated_population_2019,
@@ -41,6 +49,35 @@ last(br, 5)
 infected = br[:, :new_confirmed];
 i₀ = first(br[:, :new_confirmed]);
 N = first(br[:, :estimated_population_2019]);
+
+
+# The Susceptible-Infected-Recovered (SIR) model splits
+# the population in three time-dependent compartments:
+# the susceptible, the infected (and infectious), and the
+# recovered (and not infectious) compartments. When a susceptible individual comes into contact with an infectious individual,
+# the former can become infected for some time, and then recover and become immune. The dynamics can be summarized in a system ODEs:
+
+# ![SIR Model](/pages/images/SIR.png)
+#
+# \center{*Susceptible-Infected-Recovered (SIR) model*} \\
+
+# $$\begin{aligned}
+#  \frac{dS}{dt} &= -\beta  S \frac{I}{N}\\
+#  \frac{dI}{dt} &= \beta  S  \frac{I}{N} - \gamma  I \\
+#  \frac{dR}{dt} &= \gamma I
+# \end{aligned}$$
+
+# where
+
+# *  $S(t)$ is the number of people susceptible to becoming infected (no immunity),
+
+# *  $I(t)$ is the number of people currently infected (and infectious),
+
+# *  $R(t)$ is the number of recovered people (we assume they remain immune indefinitely),
+
+# *  $\beta$ is the constant rate of infectious contact between people,
+
+# *  $\gamma$ the constant recovery rate of infected individuals.
 
 # The differential equation
 # Taken from https://github.com/epirecipes/sir-julia
@@ -77,8 +114,8 @@ Turing.setadbackend(:forwarddiff)
   l = length(infected)
   β ~ TruncatedNormal(2, 1, 0, Inf)
   γ ~ TruncatedNormal(0.4, 0.5, 0, Inf)
-  #ϕ⁻ ~ Truncated(Exponential(5), 1, 999)
-  #ϕ = 1.0 / ϕ⁻
+  # ϕ⁻ ~ Truncated(Exponential(5), 1, 999)
+  # ϕ = 1.0 / ϕ⁻
   I = i₀
   u0 = [N - I, I, 0.0] # # S,I,R
   p = [β, γ] # # β,γ
@@ -91,7 +128,7 @@ Turing.setadbackend(:forwarddiff)
               Tsit5(),
               saveat=1.0)
   sol_I = Array(sol)[3, :] # New Infected cases
-  #infected .~ NegativeBinomial.(sol_I, ϕ)
+  # infected .~ NegativeBinomial.(sol_I, ϕ)
   infected .~ Poisson.(sol_I)
 end;
 
