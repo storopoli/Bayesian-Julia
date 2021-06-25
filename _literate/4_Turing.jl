5d607bb4ffd323093ab672f13f8c67cedf3a8049
Jose Storopoli <thestoropoli@gmail.com>
2021-06-25 06:47:11 -0300

    updated error because of comment spacing

--- _literate/4_Turing.jl
+++ _literate/4_Turing.jl
@@ -107,11 +107,11 @@ using Turing
 setprogress!(false) # hide
 
 @model dice_throw(y) = begin
-    # Our prior belief about the probability of each result in a six-sided dice.
-    # p is a vector of length 6 each with probability p that sums up to 1.
+    #Our prior belief about the probability of each result in a six-sided dice.
+    #p is a vector of length 6 each with probability p that sums up to 1.
     p ~ Dirichlet(6, 1)
 
-    # Each outcome of the six-sided dice has a probability p.
+    #Each outcome of the six-sided dice has a probability p.
     y ~ filldist(Categorical(p), length(y))
 end;
 
