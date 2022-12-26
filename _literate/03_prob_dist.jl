73eb4aae07d9f6ac06fc2002b9c600fb73d41bc7
Jose Storopoli <jose@storopoli.io>
2022-12-26 08:22:26 -0300

    makie 03_prob_dist

--- _literate/03_prob_dist.jl
+++ _literate/03_prob_dist.jl
@@ -41,16 +41,20 @@
 
 # Example: a 6-sided dice.
 
-using Plots, StatsPlots, Distributions, LaTeXStrings
+using CairoMakie
+using Distributions
 
-plot(DiscreteUniform(1, 6),
-        label="6-sided Dice",
-        markershape=:circle,
+f, ax, b = barplot(
+    DiscreteUniform(1, 6);
+    axis=(;
+        title="6-sided Dice",
         xlabel=L"\theta",
         ylabel="Mass",
-        ylims=(0, 0.3)
-    )
-savefig(joinpath(@OUTPUT, "discrete_uniform.svg")); # hide
+        xticks=1:6,
+        limits=(nothing, nothing, 0, 0.3),
+    ),
+)
+save(joinpath(@OUTPUT, "discrete_uniform.svg"), f); # hide
 
 # \fig{discrete_uniform}
 # \center{*Discrete Uniform between 1 and 6*} \\
@@ -68,19 +72,23 @@ savefig(joinpath(@OUTPUT, "discrete_uniform.svg")); # hide
 
 # Example: Whether the patient survived or died or whether the customer completes their purchase or not.
 
-plot(Bernoulli(0.5),
-        markershape=:circle,
-        label=L"p=0.5",
-        alpha=0.5,
+f, ax1, b = barplot(
+    Bernoulli(0.5);
+    width=0.3,
+    axis=(;
+        title=L"p=0.5",
         xlabel=L"\theta",
         ylabel="Mass",
-        ylim=(0, 1)
-    )
-plot!(Bernoulli(0.2),
-        markershape=:circle,
-        label=L"p=0.2",
-        alpha=0.5)
-savefig(joinpath(@OUTPUT, "bernoulli.svg")); # hide
+        xticks=0:1,
+        limits=(nothing, nothing, 0, 1),
+    ),
+)
+ax2 = Axis(
+    f[1, 2]; title=L"p=0.2", xlabel=L"\theta", xticks=0:1, limits=(nothing, nothing, 0, 1)
+)
+barplot!(ax2, Bernoulli(0.2); width=0.3)
+linkaxes!(ax1, ax2)
+save(joinpath(@OUTPUT, "bernoulli.svg"), f); # hide
 
 # \fig{bernoulli}
 # \center{*Bernoulli with $p = \{ 0.5, 0.2 \}$*} \\
@@ -98,19 +106,13 @@ savefig(joinpath(@OUTPUT, "bernoulli.svg")); # hide
 
 # Example: number of heads in 5 coin flips.
 
-plot(Binomial(5, 0.5),
-        markershape=:circle,
-        label=L"p=0.5",
-        alpha=0.5,
-        xlabel=L"\theta",
-        ylabel="Mass"
-    )
-plot!(Binomial(5, 0.2),
-        markershape=:circle,
-        label=L"p=0.2",
-        alpha=0.5)
-savefig(joinpath(@OUTPUT, "binomial.svg")); # hide
-
+f, ax1, b = barplot(
+    Binomial(5, 0.5); axis=(; title=L"p=0.5", xlabel=L"\theta", ylabel="Mass")
+)
+ax2 = Axis(f[1, 2]; title=L"p=0.2", xlabel=L"\theta")
+barplot!(ax2, Binomial(5, 0.2))
+linkaxes!(ax1, ax2)
+save(joinpath(@OUTPUT, "binomial.svg"), f); # hide
 # \fig{binomial}
 # \center{*Binomial with $n=5$ and $p = \{ 0.5, 0.2 \}$*} \\
 
@@ -126,27 +128,23 @@ savefig(joinpath(@OUTPUT, "binomial.svg")); # hide
 
 # Example: Number of emails you receive daily. Number of holes you find on the street.
 
-plot(Poisson(1),
-        markershape=:circle,
-        label=L"\lambda=1",
-        alpha=0.5,
-        xlabel=L"\theta",
-        ylabel="Mass"
-    )
-plot!(Poisson(4),
-    markershape=:circle,
-    label=L"\lambda=4",
-    alpha=0.5)
-savefig(joinpath(@OUTPUT, "poisson.svg")); # hide
+f, ax1, b = barplot(
+    Poisson(1); axis=(; title=L"\lambda=1", xlabel=L"\theta", ylabel="Mass")
+)
+ax2 = Axis(f[1, 2]; title=L"\lambda=4", xlabel=L"\theta")
+barplot!(ax2, Poisson(4))
+linkaxes!(ax1, ax2)
+save(joinpath(@OUTPUT, "poisson.svg"), f); # hide
 
 # \fig{poisson}
 # \center{*Poisson with $\lambda = \{ 1, 4 \}$*} \\
 
 # ### Negative Binomial
 
-# The negative binomial distribution describes an event of the number of successes in a sequence of $n$ independent experiment(s),
-# each asking a yes-no question with probability $p$ until $k$ success(es) is obtained. Note that it becomes identical to the
-# Poisson distribution at the limit of $k \to \infty$. This makes the negative binomial a robust option to replace a Poisson
+# The negative binomial distribution describes an event of the number of failures before the $k$th success in a sequence of $n$ independent experiment(s),
+# each asking a yes-no question with probability $p$.
+# Note that it becomes identical to the Poisson distribution at the limit of $k \to \infty$.
+# This makes the negative binomial a robust option to replace a Poisson
 # distribution to model phenomena with a overdispersion* (excess expected variation in data).
 
 # The negative binomial distribution has two parameters and its notation is $\text{NB} (k, p)$ or $\text{Negative-Binomial} (k, p)$:
@@ -159,18 +157,13 @@ savefig(joinpath(@OUTPUT, "poisson.svg")); # hide
 
 # Example: Annual count of tropical cyclones.
 
-plot(NegativeBinomial(1, 0.5),
-        markershape=:circle,
-        label=L"k=1",
-        alpha=0.5,
-        xlabel=L"\theta",
-        ylabel="Mass"
-    )
-plot!(NegativeBinomial(2, 0.5),
-        markershape=:circle,
-        label=L"k=2",
-        alpha=0.5)
-savefig(joinpath(@OUTPUT, "negbinomial.svg")); # hide
+f, ax1, b = barplot(
+    NegativeBinomial(1, 0.5); axis=(; title=L"k=1", xlabel=L"\theta", ylabel="Mass")
+)
+ax2 = Axis(f[1, 2]; title=L"k=2", xlabel=L"\theta")
+barplot!(ax2, NegativeBinomial(2, 0.5))
+linkaxes!(ax1, ax2)
+save(joinpath(@OUTPUT, "negbinomial.svg"), f); # hide
 
 # \fig{negbinomial}
 # \center{*Negative Binomial with $p=0.5$ and $r = \{ 1, 2 \}$*} \\
@@ -202,16 +195,16 @@ savefig(joinpath(@OUTPUT, "negbinomial.svg")); # hide
 
 # Example: Height, Weight, etc.
 
-plot(Normal(0, 1),
-        label=L"\sigma=1",
-        lw=5,
-        xlabel=L"\theta",
-        ylabel="Density",
-        xlims=(-4, 4)
-    )
-plot!(Normal(0, 0.5), label=L"\sigma=0.5", lw=5)
-plot!(Normal(0, 2), label=L"\sigma=2", lw=5)
-savefig(joinpath(@OUTPUT, "normal.svg")); # hide
+f, ax, l = lines(
+    Normal(0, 1);
+    label=L"\sigma=1",
+    linewidth=5,
+    axis=(; xlabel=L"\theta", ylabel="Density", limits=(-4, 4, nothing, nothing)),
+)
+lines!(ax, Normal(0, 0.5); label=L"\sigma=0.5", linewidth=5)
+lines!(ax, Normal(0, 2); label=L"\sigma=2", linewidth=5)
+axislegend(ax)
+save(joinpath(@OUTPUT, "normal.svg"), f); # hide
 
 # \fig{normal}
 # \center{*Normal with $\mu=0$ and $\sigma = \{ 1, 0.5, 2 \}$*} \\
@@ -234,16 +227,16 @@ savefig(joinpath(@OUTPUT, "normal.svg")); # hide
 # * Mean ($\mu$): natural logarithm of the mean the distribution
 # * Standard Deviation ($\sigma$): natural logarithm of the variance of the distribution ($\sigma^2$) is a measure of the dispersion of the observations in relation to the mean
 
-plot(LogNormal(0, 1),
-        label=L"\sigma=1",
-        lw=5,
-        xlabel=L"\theta",
-        ylabel="Density",
-        xlims=(0, 3)
-    )
-plot!(LogNormal(0, 0.25), label=L"\sigma=0.25", lw=5)
-plot!(LogNormal(0, 0.5), label=L"\sigma=0.5", lw=5)
-savefig(joinpath(@OUTPUT, "lognormal.svg")); # hide
+f, ax, l = lines(
+    LogNormal(0, 1);
+    label=L"\sigma=1",
+    linewidth=5,
+    axis=(; xlabel=L"\theta", ylabel="Density", limits=(0, 3, nothing, nothing)),
+)
+lines!(ax, LogNormal(0, 0.25); label=L"\sigma=0.25", linewidth=5)
+lines!(ax, LogNormal(0, 0.5); label=L"\sigma=0.5", linewidth=5)
+axislegend(ax)
+save(joinpath(@OUTPUT, "lognormal.svg"), f); # hide
 
 # \fig{lognormal}
 # \center{*Log-Normal with $\mu=0$ and $\sigma = \{ 1, 0.25, 0.5 \}$*} \\
@@ -259,16 +252,16 @@ savefig(joinpath(@OUTPUT, "lognormal.svg")); # hide
 
 # Example: How long until the next earthquake. How long until the next bus arrives.
 
-plot(Exponential(1),
-        label=L"\lambda=1",
-        lw=5,
-        xlabel=L"\theta",
-        ylabel="Density",
-        xlims=(0, 4.5)
-    )
-plot!(Exponential(0.5), label=L"\lambda=0.5", lw=5)
-plot!(Exponential(1.5), label=L"\lambda=2", lw=5)
-savefig(joinpath(@OUTPUT, "exponential.svg")); # hide
+f, ax, l = lines(
+    Exponential(1);
+    label=L"\lambda=1",
+    linewidth=5,
+    axis=(; xlabel=L"\theta", ylabel="Density", limits=(0, 4.5, nothing, nothing)),
+)
+lines!(ax, Exponential(0.5); label=L"\lambda=0.5", linewidth=5)
+lines!(ax, Exponential(1.5); label=L"\lambda=2", linewidth=5)
+axislegend(ax)
+save(joinpath(@OUTPUT, "exponential.svg"), f); # hide
 
 # \fig{exponential}
 # \center{*Exponential with $\lambda = \{ 1, 0.5, 1.5 \}$*} \\
@@ -292,16 +285,16 @@ savefig(joinpath(@OUTPUT, "exponential.svg")); # hide
 
 # Example: A database full of outliers.
 
-plot(TDist(2),
-        label=L"\nu=2",
-        lw=5,
-        xlabel=L"\theta",
-        ylabel="Density",
-        xlims=(-4, 4)
-    )
-plot!(TDist(8), label=L"\nu=8", lw=5)
-plot!(TDist(30), label=L"\nu=30", lw=5)
-savefig(joinpath(@OUTPUT, "tdist.svg")); # hide
+f, ax, l = lines(
+    TDist(2);
+    label=L"\nu=2",
+    linewidth=5,
+    axis=(; xlabel=L"\theta", ylabel="Density", limits=(-4, 4, nothing, nothing)),
+)
+lines!(ax, TDist(8); label=L"\nu=8", linewidth=5)
+lines!(ax, TDist(30); label=L"\nu=30", linewidth=5)
+axislegend(ax)
+save(joinpath(@OUTPUT, "tdist.svg"), f); # hide
 
 # \fig{tdist}
 # \center{*Student-$t$ with $\nu = \{ 2, 8, 30 \}$*} \\
@@ -319,16 +312,16 @@ savefig(joinpath(@OUTPUT, "tdist.svg")); # hide
 # Example: A basketball player has made already scored 5 free throws while missing 3 in a total of 8 attempts
 # -- $\text{Beta}(3, 5)$.
 
-plot(Beta(1, 1),
-        label=L"a=b=1",
-        lw=5,
-        xlabel=L"\theta",
-        ylabel="Density",
-        xlims=(0, 1)
-    )
-plot!(Beta(3, 2), label=L"a=3, b=2", lw=5)
-plot!(Beta(2, 3), label=L"a=2, b=3", lw=5)
-savefig(joinpath(@OUTPUT, "beta.svg")); # hide
+f, ax, l = lines(
+    Beta(1, 1);
+    label=L"a=b=1",
+    linewidth=5,
+    axis=(; xlabel=L"\theta", ylabel="Density", limits=(0, 1, nothing, nothing)),
+)
+lines!(ax, Beta(3, 2); label=L"a=3, b=2", linewidth=5)
+lines!(ax, Beta(2, 3); label=L"a=2, b=3", linewidth=5)
+axislegend(ax)
+save(joinpath(@OUTPUT, "beta.svg"), f); # hide
 
 # \fig{beta}
 # \center{*Beta with different values of $a$ and $b$*} \\
