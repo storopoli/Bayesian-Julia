b765991d28d54ad53743c38b29604f24478988e5
Jose Storopoli <jose@storopoli.io>
2022-12-26 11:20:57 -0300

    fix setrange in 12_Turing_tricks

--- _literate/12_Turing_tricks.jl
+++ _literate/12_Turing_tricks.jl
@@ -121,7 +121,7 @@ betas = mapslices(
     x -> R_ast^-1 * x, chain_qr[:, namesingroup(chain_qr, :β), :].value.data; dims=[2]
 )
 chain_beta = setrange(
-    Chains(betas, ["real_β[$i]" for i in 1:size(Q_ast, 2)]), 1_001:1:3_000
+    Chains(betas, ["real_β[$i]" for i in 1:size(Q_ast, 2)]), 1_001:1:2_000
 )
 chain_qr_reconstructed = hcat(chain_beta, chain_qr)
 
