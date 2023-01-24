592dacffb9e97e8cb766d84e8cbe98ec1c5f4b47
Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>
2023-01-24 05:02:39 +0100

    Fix typos (#75)
    * Fix typos
    
    * Update index.md
    
    Co-authored-by: Jose Storopoli <jose@storopoli.io>

--- _literate/01_why_Julia.jl
+++ _literate/01_why_Julia.jl
@@ -445,7 +445,7 @@
 
 # ## Multiple Dispatch
 
-# I think that this is the **real gamechanger of Julia language**:
+# I think that this is the **real game changer of Julia language**:
 # The ability to define **function behavior** across many combinations of argument types via [**multiple dispatch**](https://en.wikipedia.org/wiki/Multiple_dispatch).
 # **Multiple dispatch** is a feature that allows a function or method to be **dynamically dispatched** based on the run-time (dynamic) type or,
 # in the more general case, some other attribute of more than one of its arguments.
